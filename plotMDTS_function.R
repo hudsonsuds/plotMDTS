9d9528ebe7c5d8dc691f37e34e01767581560c2a
Matthew Hudson <matt.b.hudson@gmail.com>
2015-06-01 14:47:18 -0700

    Add weekends, zeros, and fix a bunch of bugs
    Add support for:
    - Weekend highlighting
    - Fill in zeros for incomplete dataset
    - Using “n(metric)” to signify counting uniques instead of a sum of the
    metric
    - Use a zero-based y-axis
    - Add marks/points for each value on line
    
    Fix bugs for:
    - Lazy eval of variable names for various dplyr functions

--- plotMDTS_function.R
+++ plotMDTS_function.R
@@ -1,7 +1,8 @@
 require(ggplot2)
 require(scales)
 require(dplyr)
-require(assertthat)
+require(lazyeval)
+require(stringr)
 
 #' Plot a Multi-Dimensional Time-Series
 #' 
@@ -10,6 +11,8 @@ require(assertthat)
 #' @param data.in A time-series data frame with a column of dates named "date"
 #' @param metric The metric you want to plot (required)
 #' @export
+#' @examples plotMDTS(example_data, metric="raisedAmt", filters = ~date > '2007-01-01',
+#'                    group = "category", facet = "state")
 #' 
 
 plotMDTS <- function(
@@ -20,11 +23,24 @@ plotMDTS <- function(
   facet = NULL,
   by.date = "Daily",
   title = metric,
-  y_axis = metric,
-  x_axis = "Date"
+  ylab = metric,
+  xlab = "Date",
+  weekends = FALSE,
+  add.zeros = FALSE,
+  counting = FALSE,
+  y.zero = FALSE
 ) {
   
-  # Verify key columns 
+  # Convert data.in into a data frame (so group operaitons work)
+  data.in <- as.data.frame(data.in)
+  
+  # Check for count of metric
+  if(grepl("n\\(.*\\)", metric)) {
+    counting <- TRUE
+    metric <- substr(metric, 3, nchar(metric) - 1)
+  }
+  
+  # Verify columns 
   stopifnot(exists("date", where = data.in))
   stopifnot(exists(metric, where = data.in))  
   
@@ -60,34 +76,71 @@ plotMDTS <- function(
     # Neither
     data.group <- group_by(data.clean, date)
     
-  } 
-  data.group <- summarize_(data.group,
-                           value = paste0("sum(", metric,", na.rm=TRUE)"))
+  }
+  
+  # Add metric
+  if(counting) {
+    data.group <- summarize_(data.group,
+                            value = interp(~n_distinct(var), var = as.name(metric)))
+    
+  } else {
+    data.group <- summarize_(data.group,
+                             value = interp(~sum(var, na.rm = TRUE), 
+                                            var = as.name(metric)))
+  }
   
   # Rename value to metric
+  if(y.zero) {
+    min.val <- min(0, min(data.group$value, na.rm = TRUE))
+  } else {
+    min.val <- min(data.group$value, na.rm = TRUE)
+  }
+  max.val <- max(data.group$value, na.rm = TRUE)
   colnames(data.group)[colnames(data.group)=="value"] <- metric
   
-  # TODO(mbh): Fill zeros for segments missing data
-  # TODO(mbh): Add support for weekend highlighting
+  # Fill in zeros for missing values
+  if(add.zeros) {
+    data.group <- addMissingZeros(data.group)
+    
+  }
   
   # Create plot
-  data.plot <- ggplot() +     
-    ggtitle(title) +
-    scale_y_continuous(breaks = pretty_breaks()) +
-    ylab(y_axis) + 
-    xlab(x_axis) + 
-    theme_bw() + 
-    theme(legend.title=element_blank()) +
-    scale_colour_brewer(palette="Set1")
+  data.plot <- ggplot()
+  
+  # Add weekends
+  if(weekends) {
+    # Calculate weekends for plotting     
+    weekends <- select(
+      filter(data.frame(date=seq(min(data.group$date, na.rm = TRUE), 
+                                 max(data.group$date, na.rm = TRUE), by="day")), 
+             weekdays(date) == "Saturday"), 
+      date)    
+    plot.weekends <- data.frame(xmin = weekends -.5,
+                                xmax = weekends + 1.5,
+                                ymin = min.val,
+                                ymax = max.val)
+    names(plot.weekends) <- c("xmin", "xmax", "ymin", "ymax")
+    
+    #Add weekends to plot
+    data.plot <- data.plot +
+      geom_rect(data=plot.weekends, aes(xmin=xmin, xmax=xmax, ymin=ymin, 
+                                        ymax=ymax),
+                fill = "grey80", alpha = "0.15")
+  }
   
   # Lines to plot
   if(!is.null(group)) {
     # Adding group color
-    data.plot <- data.plot + geom_line(data = data.group, aes_string(x = "date", y = metric, color = group))
+    data.plot <- data.plot + 
+      geom_line(data = data.group, aes_string(x = "date", y = metric, color = group)) +
+      geom_point(data = data.group, aes_string(x = "date", y = metric, color = group))
+    
     
   } else {
     # No group
-    data.plot <- data.plot + geom_line(data = data.group, aes_string(x = "date", y = metric))
+    data.plot <- data.plot + 
+      geom_line(data = data.group, aes_string(x = "date", y = metric)) +
+      geom_point(data = data.group, aes_string(x = "date", y = metric))
     
   }
   
@@ -96,8 +149,77 @@ plotMDTS <- function(
     # Add facet
     data.plot <- data.plot + facet_wrap(as.formula(paste0("~", facet)))
     
-  }
+  }  
+  
+  # Add styling
+  data.plot <- data.plot + 
+    ggtitle(title) +
+    scale_y_continuous(breaks = pretty_breaks()) +
+    ylab(ylab) + 
+    xlab(xlab) + 
+    theme_bw() + 
+    theme(legend.title=element_blank())
   
   # Return plot
   return(data.plot)
+}
+
+
+#' Replace missing data points for time series with zeros
+#' 
+#' This function takes a multi-dimensional data frame and passes
+#' back an identical one but with rows added for dates missing
+#' values for each unique combination of dimensions in the dataframe
+#' that has existing values (e.g. it won't complete for already
+#' empty combinations).
+
+addMissingZeros <- function(data.in) {  
+  
+  # Convert to data frame
+  data.in <- as.data.frame(data.in)
+  
+  # Verify key columns 
+  stopifnot(exists("date", where = data.in))
+  
+  # Remove date from data frame and convert from being grouped
+  data.nodate <- as.data.frame(select(data.in, -date))
+  
+  # Determine unique combination of dimensions of data frame
+  dimensions <- unique(data.nodate[!sapply(data.nodate, is.numeric)])
+  
+  # Possible dates of data frame
+  possible.dates <- seq(min(data.in$date, na.rm = TRUE),
+                        max(data.in$date, na.rm = TRUE),
+                        by = "day")
+  
+  # Verify size of join (don't allow huge joins)
+  resulting.size <- length(possible.dates) * length(dimensions)
+  if (resulting.size > 2000000) {
+    stop("Adding zeros for an enormous number of rows")
+  }
+  
+  # Create sparse data frame of possible values
+  if (nrow(dimensions) == 0 ) {
+    
+    # If there are no dimensions beyond the date and metric
+    possible.rows <- data.frame(date = possible.dates)
+    
+  } else {
+    possible.rows <- merge(
+      data.frame(date = possible.dates),
+      dimensions,
+      all = TRUE)
+    
+  }
+  
+  # Merge with actual data frame
+  data.out <- left_join(
+    possible.rows,
+    data.in)
+  
+  # Replace NAs w/ Zeros 
+  # TODO(mbh): Fix to only do this for metric columns
+  data.out[is.na(data.out)] <- 0
+  
+  return(data.out)
 }
